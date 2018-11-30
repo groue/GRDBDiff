a96e4b1f60bef1cc93c9c8dba77f94c21a7a66f5
Gwendal Roué <gr@pierlis.com>
2018-11-30 18:10:18 +0100

    Cleanup & Documentation

--- Documentation/DemoApp/DemoApp/PlacesViewController.swift
+++ Documentation/DemoApp/DemoApp/PlacesViewController.swift
@@ -4,9 +4,8 @@ import GRDB
 import GRDBDiff
 
 class PlacesViewController: UIViewController {
-    private var annotationsObserver: TransactionObserver?
-    
     @IBOutlet private var mapView: MKMapView!
+    private var annotationsObserver: TransactionObserver?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +17,110 @@ class PlacesViewController: UIViewController {
         super.viewWillAppear(animated)
         navigationController?.setToolbarHidden(false, animated: animated)
         zoomOnPlaces(animated: false)
+    }
+}
+
+extension PlacesViewController: MKMapViewDelegate {
+    
+    // MARK: - Map View
+    
+    private func setupMapView() {
+        // We want to track the sets of inserted, deleted, and updated places,
+        // so that we can update the mapView accordingly.
+        //
+        // We'll use the ValueObservation.trackingSetDifferences(...) method
+        // provided by GRDBDiff.
+        //
+        // Its first parameter is a request of observed records, ordered by
+        // primary key. We have made our PlaceAnnotation a GRDB record, so
+        // the request is easy to build:
+        let annotations = PlaceAnnotation.orderByPrimaryKey()
+        
+        // Now let's set up the database observation.
+        //
+        // Map view annotations need a special care: when annotations are
+        // modified, it is not a good practice to remove the old version from
+        // the map, and add the new version. It does not provide good visual
+        // results. Instead, it is better to modify the annotation in place: the
+        // MKMapView uses Key-Value Observation in order to update the
+        // annotation view accordingly.
+        //
+        // Such modifications of annotations must happen on the
+        // main thread.
+        //
+        // So let's ask the observation to reuse an annotation when it is
+        // modified: this is the `updateElement` parameter. But don't update
+        // the annotation right away: wait until we are back on the main queue
+        // before we modify the annotation on the map (this will happen in
+        // updateMapView(with:) below.
+        let annotationObservation = ValueObservation.trackingSetDifferences(
+            in: annotations,
+            updateElement: { annotation, row in
+                annotation.nextPlace = Place(row: row)
+                return annotation
+        })
+        
+        // Start the database observation.
+        annotationsObserver = try! annotationObservation.start(in: dbPool) { [weak self] diff in
+            self?.updateMapView(with: diff)
+        }
+    }
+    
+    private func updateMapView(with diff: SetDifferences<PlaceAnnotation>) {
+        mapView.addAnnotations(diff.inserted)
+        mapView.removeAnnotations(diff.deleted)
+        for annotation in diff.updated {
+            // Modify the annotation now, on the main thread.
+            // See setupMapView() for a longer explanation.
+            annotation.place = annotation.nextPlace!
+        }
+        
+        zoomOnPlaces(animated: true)
+    }
+    
+    private func zoomOnPlaces(animated: Bool) {
+        // Turn all annotations into zero-sized map rects, that we will union
+        // to build the zooming map rect.
+        let rects = mapView.annotations.map { annotation in
+            MKMapRect(
+                origin: MKMapPoint(annotation.coordinate),
+                size: MKMapSize(width: 0, height: 0))
+        }
+        
+        // No rect => no annotation => no zoom
+        guard let firstRect = rects.first else {
+            return
+        }
+        
+        // Union rects
+        let zoomRect = rects
+            .suffix(from: 1)
+            .reduce(firstRect) { $0.union($1) }
+        
+        // Zoom
+        mapView.setVisibleMapRect(
+            zoomRect,
+            edgePadding: UIEdgeInsets(top: 40, left: 40, bottom: 40, right: 40),
+            animated: animated)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let view = mapView.dequeueReusableAnnotationView(withIdentifier: "annotation") {
+            return view
+        }
+        let view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "annotation")
+        view.displayPriority = .required // opt out of clustering in order to show *all* annotations
+        return view
+    }
+    
+    private func findPlaceAnnotation(id: Int64?) -> PlaceAnnotation? {
+        for annotation in mapView.annotations {
+            if let placeAnnotation = annotation as? PlaceAnnotation,
+                placeAnnotation.place.id == id {
+                return placeAnnotation
+            }
+        }
+        return nil
     }
 }
 
@@ -79,77 +182,3 @@ extension PlacesViewController {
     }
 }
 
-extension PlacesViewController: MKMapViewDelegate {
-    
-    // MARK: - Map View
-    
-    private func setupMapView() {
-        let annotations = PlaceAnnotation.orderByPrimaryKey()
-        
-        let annotationObservation = ValueObservation.trackingSetDifferences(
-            in: annotations,
-            updateElement: { annotation, row in
-                annotation.nextPlace = Place(row: row)
-                return annotation
-        })
-        
-        annotationsObserver = try! annotationObservation.start(in: dbPool) { [weak self] diff in
-            self?.updateMapView(with: diff)
-        }
-    }
-    
-    private func updateMapView(with diff: SetDifferences<PlaceAnnotation>) {
-        mapView.addAnnotations(diff.inserted)
-        mapView.removeAnnotations(diff.deleted)
-        for updatedAnnotation in diff.updated {
-            updatedAnnotation.place = updatedAnnotation.nextPlace!
-        }
-        
-        zoomOnPlaces(animated: true)
-    }
-    
-    private func zoomOnPlaces(animated: Bool) {
-        // Turn all annotations into zero-sized map rects, that we will union
-        // to build the zooming map rect.
-        let rects = mapView.annotations.map { annotation in
-            MKMapRect(
-                origin: MKMapPoint(annotation.coordinate),
-                size: MKMapSize(width: 0, height: 0))
-        }
-        
-        // No rect => no annotation => no zoom
-        guard let firstRect = rects.first else {
-            return
-        }
-        
-        // Union rects
-        let zoomRect = rects
-            .suffix(from: 1)
-            .reduce(firstRect) { $0.union($1) }
-        
-        // Zoom
-        mapView.setVisibleMapRect(
-            zoomRect,
-            edgePadding: UIEdgeInsets(top: 40, left: 40, bottom: 40, right: 40),
-            animated: animated)
-    }
-    
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        if let view = mapView.dequeueReusableAnnotationView(withIdentifier: "annotation") {
-            return view
-        }
-        let view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "annotation")
-        view.displayPriority = .required // opt out of clustering in order to show *all* annotations
-        return view
-    }
-    
-    private func findPlaceAnnotation(id: Int64?) -> PlaceAnnotation? {
-        for annotation in mapView.annotations {
-            if let placeAnnotation = annotation as? PlaceAnnotation,
-                placeAnnotation.place.id == id {
-                return placeAnnotation
-            }
-        }
-        return nil
-    }
-}
